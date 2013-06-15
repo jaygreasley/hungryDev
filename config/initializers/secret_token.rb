9aeed1d5f4e4d415da466bbe7c0df994c64aa940
jay greasley <jaygreasley@gmail.com>
2013-06-15 10:42:37 +0000

    init - with country scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = 'cef88c67df3047465279619f6f3e9aca3a86878062c79a013f1d63ec65c9f6f33251f16f1eedab8f05de74f0e6d75d165837433234ca9a19ca367ba4d9f28517'
